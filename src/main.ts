a8efe2b04aef79c58472b17eeb3765816d945df1
EkyGalih <ekkygalih8@gmail.com>
2025-09-15 15:40:10 +0800

    ADD::jwt

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,29 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  
+  // atcivate CORS
+  app.enableCors({
+    origin: [
+      'http://localhost:3000',
+    ],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  })
+
+  // swagger config
+  const config = new DocumentBuilder()
+  .setTitle('Pendataan API')
+  .setDescription('Documentation API for app pendataan')
+  .setVersion('1.0')
+  .addBearerAuth()
+  .build();
+  
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document);
+  await app.listen(process.env.PORT ?? 8000);
 }
 bootstrap();
